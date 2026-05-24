6cb4ae7f8ebbd2c82fbe6a66cfa8547944dbc958
akumasan <ankurvr288@gmail.com>
2026-05-25 00:01:39 +0530

    Implement JWT-based user authentication

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
     try{
@@ -34,6 +35,60 @@ export const register = async (req, res) => {
         });
 
     }catch(error){
+        console.error(error);
+
+        res.status(500).json({
+            message: "Server error",
+        });
+    }
+};
+
+
+export const login = async (req, res) => {
+    try{
+        const {identifier, password} = req.body;
+
+        if(!identifier || !password){
+            return res.status(400).json({
+                message: "All fields are required",
+            });
+        }
+
+        const user = await User.findOne({
+            $or: [
+                {email: identifier},
+                {username: identifier},
+            ],
+        });
+
+        if(!user) {
+            return res.status(401).json({
+                message: "Invalid credentials",
+            });
+        }
+
+        const isMatch = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if(!isMatch) {
+            return res.status(401).json({
+                message: "Invalid credentials",
+            });
+        }
+
+        const token = jwt.sign(
+            { userId: user._id, },
+            process.env.JWT_SECRET,
+            { expiresIn: "7d" }
+        );
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+        });
+    } catch (error) {
         console.error(error);
 
         res.status(500).json({
